e04212497822eada42ce91b6a08bd4dbcfd926f7
Kristen Scheel <kristen.scheel@gmail.com>
2020-08-18 14:52:05 -0400

    Updates

---
+++ precache-manifest.56dc5ab65fdf623d5c26c47a6c3775aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f949c14b59a01028009c24c5acd2b0b",
+    "url": "/kristenAymenSaudProject6ix/index.html"
+  },
+  {
+    "revision": "70aa7df7314269efbf0e",
+    "url": "/kristenAymenSaudProject6ix/static/css/main.3c4b3767.chunk.css"
+  },
+  {
+    "revision": "951cce1bf2269e482f7a",
+    "url": "/kristenAymenSaudProject6ix/static/js/2.b28eb481.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/kristenAymenSaudProject6ix/static/js/2.b28eb481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70aa7df7314269efbf0e",
+    "url": "/kristenAymenSaudProject6ix/static/js/main.1045a5ca.chunk.js"
+  },
+  {
+    "revision": "6c0dcd51d8b03ea27c07",
+    "url": "/kristenAymenSaudProject6ix/static/js/runtime-main.bfc1a752.js"
+  }
+]);
